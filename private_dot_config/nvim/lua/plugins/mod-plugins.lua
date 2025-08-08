300e446ff2065e168ea7326b32c8a5c8db762514
N S <nischalshrestha314@gmail.com>
2025-08-08 19:28:33 +0545

    Change theme to Gruvbox and modify indents

--- private_dot_config/nvim/lua/plugins/mod-plugins.lua
+++ private_dot_config/nvim/lua/plugins/mod-plugins.lua
@@ -1,8 +1,11 @@
 return {
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "gruvbox",
     },
   },
   {
@@ -22,7 +25,7 @@ return {
       -- create the highlight groups in the highlight setup hook, so they are reset
       -- every time the colorscheme changes
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#EA860B" })
         vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
         vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
       end)
