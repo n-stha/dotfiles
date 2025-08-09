aba6441d6d02c1448731cede019a8c29815955b4
N S <nischalshrestha314@gmail.com>
2025-08-09 14:18:04 +0545

    Remove files from chezmoi config

--- private_dot_config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-vim.api.nvim_create_autocmd("ExitPre", {
-	group = vim.api.nvim_create_augroup("Exit", { clear = true }),
-	command = "set guicursor=a:ver100-blinkwait700-blinkoff400-blinkon250",
-})
