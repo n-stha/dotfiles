9c570404012536462f0c4e3f71ec4996d666c959
N S <nischalshrestha314@gmail.com>
2025-08-27 10:02:54 +0545

    Add Neovide font config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,9 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.api.nvim_create_autocmd("ExitPre", {
-	group = vim.api.nvim_create_augroup("Exit", { clear = true }),
-	command = "set guicursor=a:ver100-blinkwait700-blinkoff400-blinkon250",
+  group = vim.api.nvim_create_augroup("Exit", { clear = true }),
+  command = "set guicursor=a:ver100-blinkwait700-blinkoff400-blinkon250",
 })
+
+if vim.g.neovide then
+  vim.o.guifont = "MonaspiceRn Nerd Font Mono:h12"
+end
